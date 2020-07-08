d32143e80d1640a723fc76663424d0688a0053dd
Yealink89 <782491165@qq.com>
2020-07-08 10:43:17 +0800

    新增折叠页面

--- flutter_app/lib/Pages/search_page.dart
+++ flutter_app/lib/Pages/search_page.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-
-class SearchPage extends StatefulWidget{
-  @override
-  _SearchState createState() => _SearchState();
-}
 const CITY_NAMES = {
   '北京': ['东城区', '西城区', '朝阳区', '丰台区', '石景山区', '大兴区', '海淀区'],
   '上海': ['黄浦区', '徐汇区', '长宁区', '普陀区', '虹口区', '闸北区', '静安区'],
@@ -12,13 +7,53 @@ const CITY_NAMES = {
   '苏州': ['姑苏区', '吴中区', '相城区', '拱墅区', '高新区', '虎丘区', '吴江区']
 };
 
+class SearchPage extends StatefulWidget{
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+
 class _SearchState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+    final title = '列表展开与收起';
     return Scaffold(
-      body: Center(
-        child: Text('搜索'),
+      body: Container(
+        child: ListView(
+          children: _buildList(),
+        ),
       ),
     );
   }
 }
+
+List<Widget> _buildList(){
+  List<Widget> widgets = [];
+  CITY_NAMES.keys.forEach((key) {
+    widgets.add(_item(key,CITY_NAMES[key]));
+  });
+  return widgets;
+}
+
+Widget _item(String city,List<String> subCities){
+  return ExpansionTile(
+    title: Text(
+      city,
+      style: TextStyle(color: Colors.black54,fontSize: 20)
+    ),
+    children: subCities.map((subCity) => _buildSub(subCity)).toList(),
+  );
+}
+
+Widget _buildSub(String subCity){
+  return FractionallySizedBox(
+    widthFactor: 1,
+    child: Container(
+      height: 50,
+      margin: EdgeInsets.only(bottom: 5),
+      decoration: BoxDecoration(color: Colors.lightBlueAccent),
+      child: Text(subCity),
+    ),
+  );
+}
+
