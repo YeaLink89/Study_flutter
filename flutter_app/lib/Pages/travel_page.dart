63569c623c02a3b609c3074eaa14b1b086ee98b9
YeaLink89 <782491165@qq.com>
2020-07-09 11:21:51 +0800

    Update travel_page.dart
    新增下拉刷新样式

--- flutter_app/lib/Pages/travel_page.dart
+++ flutter_app/lib/Pages/travel_page.dart
@@ -5,22 +5,33 @@ class TravelPage extends StatefulWidget{
   _TravelState createState() => _TravelState();
 }
 
-const CITY_NAMES = ['北京','上海','广州','深圳','杭州','苏州','成都','武汉','郑州'];
+List<String> cityNames = ['北京','上海','广州','深圳','杭州','苏州','成都','武汉','郑州'];
 class _TravelState extends State<TravelPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: _buildList(),
+      body: RefreshIndicator(
+          child:ListView(
+            scrollDirection: Axis.vertical,
+            children: _buildList(),
+          ),
+          onRefresh: _handleRefresh,
       ),
     );
   }
+
+  //模拟下拉刷新功能
+  Future _handleRefresh() async{
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      cityNames = cityNames.reversed.toList();
+    });
+    return null;
+  }
 }
 
-List<Widget>_buildList()
-{
-  return CITY_NAMES.map((city) => _item(city)).toList();
+List<Widget>_buildList() {
+  return cityNames.map((city) => _item(city)).toList();
 }
 
 Widget _item(String city){
